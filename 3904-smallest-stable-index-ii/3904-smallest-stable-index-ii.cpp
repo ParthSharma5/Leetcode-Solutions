8166da13c9100d3523232646a52468412d9ed8fa
PARTH SHARMA <85158842+ParthSharma5@users.noreply.github.com>
2026-09-05 16:45:18 +0530

    Time: 2 ms (95.72%), Space: 202.7 MB (76.32%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -3,16 +3,15 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
         int minEle = INT_MAX;
-        vector<int>minIndex(n);
+        vector<int>stable(n);
         for(int i=n-1;i>=0;i--){
             minEle = min(minEle,nums[i]);
-            minIndex[i] = minEle;
+            stable[i] = minEle;
         }
         int maxEle = INT_MIN;
         for(int i=0;i<n;i++){
-            maxEle = max(maxEle , nums[i]);
-            if(maxEle - minIndex[i] <= k)
-            {   
+            maxEle = max(maxEle,nums[i]);
+            if(maxEle - stable[i] <= k){
                 return i;
             }
         }
